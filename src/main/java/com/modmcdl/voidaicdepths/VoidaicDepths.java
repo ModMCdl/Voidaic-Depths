80b4d1076456827406cd6fc568354ea2cc46c0d3
ModMCdl <modmcdl@gmail.com>
2021-07-21 17:16:04 -0400

    Added json worldgen. Needs work.

--- src/main/java/com/modmcdl/voidaicdepths/VoidaicDepths.java
+++ src/main/java/com/modmcdl/voidaicdepths/VoidaicDepths.java
@@ -4,7 +4,6 @@ import com.modmcdl.voidaicdepths.core.init.VoidaicBlocks;
 import com.modmcdl.voidaicdepths.core.init.VoidaicItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -13,6 +12,8 @@ public class VoidaicDepths implements ModInitializer
 {
 	public static final String MOD_ID = "voidaicdepths";
 	public static final ItemGroup VD_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "main"), () -> new ItemStack(VoidaicBlocks.VOID_LANTERN.asItem()));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -21,6 +22,8 @@ public class VoidaicDepths implements ModInitializer
 
 		VoidaicItems.register(); //Register Items
 		VoidaicBlocks.register(); //Register Blocks
+
+
 
 		System.out.println("Voidaic Depths checking in!");
 	}
--- src/main/java/com/modmcdl/voidaicdepths/core/init/VoidaicItems.java
+++ src/main/java/com/modmcdl/voidaicdepths/core/init/VoidaicItems.java
@@ -12,8 +12,16 @@ public class VoidaicItems
     public static final LinkedHashMap<Item, Identifier> ITEMS = new LinkedHashMap<>();
 
     //-- ITEM LIST --//
-    public static final Item VOID_ESSENCE = create("void_essence", new Item(itemGroup()));
+    public static final Item ARCANE_SALT = create("arcane_salt", new Item(group()));
+    public static final Item VOID_ESSENCE = create("void_essence", new Item(group()));
+    public static final Item VOID_CRYSTAL = create("void_crystal", new Item(group()));
+    public static final Item ATTUNED_ENDER_PEARL = create("attuned_ender_pearl", new Item(group()));
 
+    public static final Item VOID_INGOT = create("void_ingot", new Item(group()));
+    public static final Item VOID_NUGGET = create("void_nugget", new Item(group()));
+
+    public static final Item TRANSLOCATION_STAFF = create("staff_of_translocation", new Item(group()));
+    public static final Item VOID_GOGGLES = create("void_goggles", new Item(group()));
 
     public static void register()
     {
@@ -26,7 +34,7 @@ public class VoidaicItems
         return item;
     }
 
-    public static Item.Settings itemGroup()
+    public static Item.Settings group()
     {
         return new Item.Settings().group(VoidaicDepths.VD_GROUP);
     }
