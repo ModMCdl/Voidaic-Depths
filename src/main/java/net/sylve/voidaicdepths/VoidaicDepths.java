76bf544b5656cda79d820e998535c99ff45b6536
ModMCdl <modmcdl@gmail.com>
2022-05-26 20:11:22 -0400

    1.18.2 Port
    Delete old gradle

--- src/main/java/net/sylve/voidaicdepths/VoidaicDepths.java
+++ src/main/java/net/sylve/voidaicdepths/VoidaicDepths.java
@@ -11,9 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VoidaicDepths implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 
 	public static final String MODID = "voidaicdepths";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
