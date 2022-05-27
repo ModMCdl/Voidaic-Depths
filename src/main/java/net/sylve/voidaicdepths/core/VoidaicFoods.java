6fda2069264a1e374d366a48dcbe01a96f596748
ModMCdl <modmcdl@gmail.com>
2022-05-26 20:14:02 -0400

    Reset
    Remove 1.17 - clear repo

--- src/main/java/net/sylve/voidaicdepths/core/VoidaicFoods.java
+++
@@ -1,8 +0,0 @@
-package net.sylve.voidaicdepths.core;
-
-import net.minecraft.item.FoodComponent;
-
-public class VoidaicFoods
-{
-    public static final FoodComponent VOID_CARROT = (new FoodComponent.Builder()).hunger(6).saturationModifier(1.0F).build();
-}
--- src/main/java/net/sylve/voidaicdepths/core/VoidaicSignTypes.java
+++
@@ -1,9 +0,0 @@
-package net.sylve.voidaicdepths.core;
-
-import net.minecraft.util.SignType;
-import net.sylve.voidaicdepths.mixin.SignTypeAccessor;
-
-public class VoidaicSignTypes
-{
-    public static final SignType UNDERWOOD = SignTypeAccessor.registerNew(SignTypeAccessor.newSignType("underwood"));
-}
